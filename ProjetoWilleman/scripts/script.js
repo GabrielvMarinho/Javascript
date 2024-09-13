d534c55c68369ba6c35f39543f20e7a979d3cdf1
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-09-12 21:21:45 -0300

    varios paineis

--- ProjetoWilleman/scripts/script.js
+++ ProjetoWilleman/scripts/script.js
@@ -17,9 +17,11 @@ let listaMaquinas =[new Maquina("MAQUINA 1", "polarArea"),
 server.addlistaMaquina(listaMaquinas)
 //criando objeto Painel e criando graficos
 const painel = new PainelDeControle("Painel do gabi", server.getlistaMaquinas())
+const painel1 = new PainelDeControle("Painel do gabis", server.getlistaMaquinas())
 
 //adiciona paineis ao server
 server.adicionarPainel(painel)
+server.adicionarPainel(painel1)
 
 function atualizarTemperaturas() {
     //FUNÇÃO OBSERVER -> chama todo os objetos na lista de paineis de server e manda os dados para eles
