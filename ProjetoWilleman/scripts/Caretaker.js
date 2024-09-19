e4a90d39c895dc835866487c9ff844efdfb72d93
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-09-19 08:40:39 -0300

    comentarios bi

--- ProjetoWilleman/scripts/Caretaker.js
+++ ProjetoWilleman/scripts/Caretaker.js
@@ -1,31 +1,32 @@
 export class Caretaker{
-    constructor(nome){
-        this.nome = nome
-        this.listaMementos = []
+    constructor(nome){ // construtor recebe um parâmetro "nome"
+        this.nome = nome // o parâmetro é armazenado na propriedade this.nome
+        this.listaMementos = [] // array vazio, instancia objetos 
     }
     
-    save(memento){
-        if(this.listaMementos.length>=15){
-            this.listaMementos.shift()
-            this.listaMementos.push(memento)
+    save(memento){ // recebe um parâmetro que é um objeto
+        if(this.listaMementos.length>=15){  // verifica o comprimento da lista 
+            this.listaMementos.shift()  // se for, remove o primeiro elemento da lista (o maias antigo), 
+            // deixando espaço para adicionar um novo
+            this.listaMementos.push(memento)  // adiciona um elemento no final da lista
         }
         else{
-            this.listaMementos.push(memento)
+            this.listaMementos.push(memento) // se tiver menos, só vai adicionar um novo memento no final da fila
         }
     }
     getListaMementos(){
-        console.clear()
-        var string = ""
-        this.listaMementos.forEach(elemento =>{
-            string+=elemento.getEstado()+"\n\n"
+        console.clear() // começa limpando o console
+        var string = "" // variável inicializada como vazia
+        this.listaMementos.forEach(elemento =>{ // itera sobre cada elemento na lista memento
+            string+=elemento.getEstado()+"\n\n" // adiciona o resultado do getEstado na variável string
         })
-        return string
+        return string // retorna a string
     }
     
-    getIndex(id){
+    getIndex(id){  // o metodo retorna o memento que esta localizado no índice da lista de mementos
         return this.listaMementos[id]
     }
     getAll(){
-        return this.listaMementos
+        return this.listaMementos // o metodo retorna a lista de mementos
     }
 }
